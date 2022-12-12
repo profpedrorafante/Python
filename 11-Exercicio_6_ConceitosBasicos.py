624c8a7ada030cb4b8980a0cefcbc501893f0903
Pedro Rafante <pedrorafante@gmail.com>
2022-12-12 00:06:07 -0300

    Alterado para colocar o enunciado do programa

--- 11-Exercicio_6_ConceitosBasicos.py
+++ 11-Exercicio_6_ConceitosBasicos.py
@@ -1,5 +1,8 @@
+# 5. Faça um programa que calcule o valor de desconto para um determinado produto,
+# faça um Print Screen do código fonte do seu programa e também do programa em execução.
+
 valor = float(input('Informe o valor do produto: '))
 desconto = int(input('Informe o o valor(%) de desconto do produto: '))
 
 print('Valor do produto R$', valor, '\n Valor de desconto ', desconto, '%', '\n')
-print('Valor total com o desconto: R$', valor-(valor*(desconto/100)))
+print('Valor total com o desconto: R$', valor - (valor * (desconto / 100)))
