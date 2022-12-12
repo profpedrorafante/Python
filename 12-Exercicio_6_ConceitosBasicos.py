0e1a21d37479a61d1fe134cf47dae28d4aa919aa
Pedro Rafante <pedrorafante@gmail.com>
2022-12-12 00:09:15 -0300

    Adicionado o Exercicio 6

---
+++ 12-Exercicio_6_ConceitosBasicos.py
@@ -0,0 +1,8 @@
+# Crie um programa para converter uma temperatura da escala Celsius para a escala Fahrenheit.
+# A fórmula para conversão é Fahrenheit = ((Celsius * 9) / 5 ) + 32
+
+celsius = float(input('Informe o valor em Celsius para fazermos a conversão para Fahrenheit: '))
+fahrenheit = ((celsius * 9) / 5 ) + 32
+
+print('======== Conversão Celsius -> Fahrenheit\n', fahrenheit)
+
